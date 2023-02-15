f5311329ef2733770661efedda34c331e291090e
Shakir-Ahammed <shuvocsevu231@gmail.com>
2023-02-15 13:57:41 +0600

    yes

---
+++ cpptest.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define dd double
+#define ld long double
+#define sfl(n) scanf(
+%lld
+, &n
+,
+int main()
+{ 
+    return 0;
+}
